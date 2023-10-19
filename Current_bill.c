ab485d9c2239f096cceea9bb2e921af4a1863553
siva9550575696 <147416582+siva9550575696@users.noreply.github.com>
2023-10-19 10:50:32 +0530

    #include<stdio.h> int main() {     int a;     float b;     scanf("%d",&a);     if(a<=199)     {        b=(a*1.20)+100;        printf("%.2f",b);     }    else if(a>=200 && a<400)     {        b=(a*1.50)+100;        printf("%.2f",b);     }      else if(a>=400 && a<600)     {        b=(a*1.80)+(a*1.80)*15/100;        printf("%.2f",b);     }      else if(a>=600)     {        b=(a*2.0)+(a*2.0)*15/100;        printf("%.2f",b);     }
    }

---
+++ Current_bill.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    scanf("%d",&a);
+    if(a<=199)
+    {
+       b=(a*1.20)+100;
+       printf("%.2f",b);
+    }
+   else if(a>=200 && a<400)
+    {
+       b=(a*1.50)+100;
+       printf("%.2f",b);
+    }
+     else if(a>=400 && a<600)
+    {
+       b=(a*1.80)+(a*1.80)*15/100;
+       printf("%.2f",b);
+    }
+     else if(a>=600)
+    {
+       b=(a*2.0)+(a*2.0)*15/100;
+       printf("%.2f",b);
+    }
+   
+}
